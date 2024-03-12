1fc47cd9d635e77b61aa4f8dd71f771753eed6a5
balibabu <cike8899@users.noreply.github.com>
2024-03-12 18:21:13 +0800

    feat: replace background color of homepage (#50)
    * feat: replace background color of homepage
    
    * feat: add gradient background color to header of homepage

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -4,36 +4,35 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        {/* <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading> */}
-        <span className={styles.tinyTitle}>Super. Simple. Smart.</span>
-        <p className={clsx('hero__subtitle', styles.mainTitle)}>
-          The
-          <span className={styles.mainBlueTitle}> AI-Native </span>
-          Database for LLM Applications
-        </p>
-        <p className={styles.minorTitle}>
-          providing incredibly fast full-text and vector search
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className={`button button--secondary button--lg`}
-            to="/docs/build_from_source"
-          >
-            Get Started
-          </Link>
+    <>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <span className={styles.tinyTitle}>Super. Simple. Smart.</span>
+          <p className={clsx('hero__subtitle', styles.mainTitle)}>
+            The
+            <span className={styles.mainBlueTitle}> AI-Native </span>
+            Database for LLM Applications
+          </p>
+          <p className={styles.minorTitle}>
+            providing incredibly fast full-text and vector search
+          </p>
+          <div className={styles.buttons}>
+            <Link
+              className={`button button--secondary button--lg ${styles.getStatedLink}`}
+              to="/docs/build_from_source"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+        <div className={styles.rightBottomBackground}></div>
+      </header>
+      <div className={styles.leftTopBackground}></div>
+    </>
   );
 }
 
