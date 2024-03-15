46ef2ded7390fbde7fe2419df92022055b6a27c3
writinwaters <93570324+writinwaters@users.noreply.github.com>
2024-03-15 19:25:48 +0800

    Fixed a broken link (#67)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className={`button button--secondary button--lg ${styles.getStatedLink}`}
-              to="/docs/build_from_source"
+              to="/docs"
             >
               Get Started
             </Link>
