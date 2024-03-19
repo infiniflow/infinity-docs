5cf0124c09b2c984c8e30445ee7a72930d9b7999
writinwaters <93570324+writinwaters@users.noreply.github.com>
2024-03-19 15:32:04 +0800

    Enable versioning in the production environment (#72)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          disableVersioning: true,
+          disableVersioning: false,
           sidebarPath: './sidebars.js',
           editUrl:
             'https://github.com/infiniflow/infinity-docs/tree/main/website',
