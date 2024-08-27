134b1fa8f673be637670d3e05e563adbadcd8dd7
writinwaters <93570324+writinwaters@users.noreply.github.com>
2024-08-27 11:46:47 +0800

    Render sidebar hideable (#221)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -102,7 +102,7 @@ const config = {
       },
       docs: {
         sidebar: {
-          hideable: false,
+          hideable: true,
           autoCollapseCategories: false,
         },
       },
